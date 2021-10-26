18a8eae2163dcf380c9486701cee439b0f32b917
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-10-25 23:02:18 -0300

    Passando variavel de ambiente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 
 function App() {
+  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
+    .then(() => console.log("Sucesso"))
+    .catch(() => console.log("Erro"))
   return (
     <div className="App">
       <header className="App-header">
